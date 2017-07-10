ff564acdd15b0160a1b2412969d04bc63b2cdfb4
ganesh <gnsh_sh_rai@yahoo.co.in>
2017-07-10 02:31:16 -0500

    python 2.7 migrated

--- pirover_broker/pirover_broker/app.py
+++ pirover_broker/pirover_broker/app.py
@@ -83,7 +83,7 @@ def recordCmdSock_complete(sid,data):
 
 if __name__ == '__main__':
     import os
-    HOST = '0.0.0.0' #os.environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
